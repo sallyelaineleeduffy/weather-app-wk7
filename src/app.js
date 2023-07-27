be395bbeabe1d164249724410e8fbbc16ad20025
Sally Lee-Duffy <sally@thesnowinstitute.com>
2023-07-27 18:53:07 +0200

    API added and tested

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
+function displayTemperature(response) {
+  console.log(response.data.city);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+}
 
+let apiKey = "f1ef93f40de3d662400b2dt0aa8a2o91";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=London&key=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
