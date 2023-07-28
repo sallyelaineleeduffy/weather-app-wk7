2ca60f54047acb474ba837e9091794521cf40d25
Sally Lee-Duffy <sally@thesnowinstitute.com>
2023-07-28 23:00:42 +0200

    Added more JS Elements from API

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 function displayTemperature(response) {
-  console.log(response.data.city);
+  console.log(response);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let windElement = document.querySelector(".wind");
+  let humidityElement = document.querySelector(".humidity");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  humidityElement.innerHTML = response.data.temperature.humidity;
 }
 
 let apiKey = "f1ef93f40de3d662400b2dt0aa8a2o91";
