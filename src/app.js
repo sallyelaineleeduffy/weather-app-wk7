d44a5eed0940986168c1323a4501cdc159b9609d
Sally Lee-Duffy <sally@thesnowinstitute.com>
2023-07-28 23:35:38 +0200

    Update JS to reflect day and time

--- src/app.js
+++ src/app.js
@@ -1,15 +1,36 @@
+function formatTime(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day}, ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   console.log(response);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
-  let windElement = document.querySelector(".wind");
-  let humidityElement = document.querySelector(".humidity");
+  let windElement = document.querySelector("#wind");
+  let humidityElement = document.querySelector("#humidity");
+  let timeElement = document.querySelector("#time");
+  let dateElement = document.querySelector("#date");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   humidityElement.innerHTML = response.data.temperature.humidity;
+  timeElement.innerHTML = formatTime(response.data.time * 1000);
+  dateElement.innerHTML = "6th June 2023";
 }
 
 let apiKey = "f1ef93f40de3d662400b2dt0aa8a2o91";
