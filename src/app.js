ea770d096db9f96a6013628efc387eef576752da
Sally Lee-Duffy <sally@thesnowinstitute.com>
2023-07-30 22:31:05 +0200

    Added JS to Search Box and updated CSS

--- src/app.js
+++ src/app.js
@@ -57,8 +57,19 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.condition.description);
 }
 
-let apiKey = "f1ef93f40de3d662400b2dt0aa8a2o91";
-let city = "London";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "f1ef93f40de3d662400b2dt0aa8a2o91";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#form-search-box");
+  search(cityInput.value);
+}
+
+search("London");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
